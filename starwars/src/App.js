5619e6d0642b7ba0956745428bab40afc9da59df
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-05-29 22:53:45 -0400

    design width pagination SW Api

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import Character from "./components/Character"
+import axios from "axios"
+import styled from "styled-components"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +12,69 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+const [characters,setCharacters] = useState([])
+const [pageToShow, setPageToShow] = useState("https://swapi.dev/api/people/?page=1")
+
+
+useEffect(() => {
+  axios.get(pageToShow)
+  .then(response => {
+    setCharacters(response.data)
+    console.log(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+},[pageToShow])
+
+// styled-Components
+const App = styled.div`
+    text-align: center;
+`
+const Characters = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`
+const Header = styled.h1`
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+  width: 100%;
+  margin: 20px 0;
+`
+const Last = styled.button`
+  padding: 5px;
+  vertical-align: middle;
+  margin-right: 20px
+`;
+
+const Next = styled.button`
+  padding: 5px;
+  vertical-align: middle;
+  margin-left: 20px
+`;
+
+
+
+
+if (characters.length !== 0) {
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+    <App>
+      <Characters>
+  <Header>{(characters.previous !== null) ? <Last onClick={()=>{setPageToShow(characters.previous)}}>{`<<`}</Last> : ""} Characters {(characters.next !== null) ? <Next onClick={()=>{setPageToShow(characters.next)}}>{`>>`}</Next> : ""}</Header>
+      {
+          characters.results.map((el) => {
+            return <Character info={el} />
+        })
+      }
+    </Characters>
+    </App>
+  )
+} else {
+  return "...Loading"
+}
+
+ 
 }
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,45 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Character = (props) => {
+  
+    // styled-components
+    const Character = styled.div`
+        width: 29%;
+        padding: 20px 30px;
+        background-color: darksalmon;
+        border-left: 25px solid #333;
+        color: #333;
+        margin-bottom: 25px;
+        text-align: left;
+    `;
+    const Name = styled.div`
+        font-size: 1.7rem;
+        margin-bottom: 20px;
+        border-bottom: 2px solid #333;
+    `;
+    const Gender = styled.span`
+        margin-left: 5px;
+        color: saddlebrown;
+    `;
+    const Paragraph = styled.p`
+        margin-bottom: 5px;
+    `;
+    
+    
+    
+  return (
+            <Character>
+                <Name>{props.info.name}<Gender>({props.info.gender})</Gender></Name>
+                <Paragraph>Hair Color:{props.info.hair_color}</Paragraph>
+                <Paragraph>Height: {props.info.height}</Paragraph>
+                <Paragraph>Mass: {props.info.mass}</Paragraph>
+                <Paragraph>Skin Color: {props.info.skin_color}</Paragraph>
+            </Character>
+           
+  );
+}
+
+export default Character;
